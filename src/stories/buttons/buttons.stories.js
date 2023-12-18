dabe9c5cffabfc7ccc537aa435a7d0036721780c
prab <prabhanjan.sharma@spreadd.io>
2023-12-18 12:09:40 +0530

    create button

---
+++ src/stories/buttons/buttons.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Buttons from "./buttons";
+
+export default {
+  title: "Buttons",
+  component: Buttons,
+};
+
+
+export const listButton = () => <Buttons />;
+// export const outlinedButton = () => <outlinedButton />;
---
+++ src/stories/theme.js
@@ -0,0 +1,21 @@
+// theme.js
+const theme = {
+  primary: "#FF4E54",
+  secondary: "#FF8242",
+  success: "#07CB6E",
+  warning: "#F3B664",
+  cardBorderColor: "#E49393",
+  cardTitleColor: "#060047",
+  cardTextColor: "#1640D6",
+  cardButtonBg: "#E90064",
+  cardButtonHoverBg: "#FF5F9E",
+  textLinkColor: "#E26EE5",
+  iconAfterLinkColor: "#ccc",
+  buttonOutlinedSecondaryBorder: "#E26EE5",
+  buttonOutlinedSecondaryBgHover: "#E26EE5",
+  buttonOutlinedSecondaryColorHover: "#fff",
+  badgeBg: "#FF3D00",
+  badgeText: "#fff",
+  white: "#f6f9fc",
+};
+export default theme;
