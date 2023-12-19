0f67f38228b1c842e3a79c290fdf5e02865c6c18
prab <prabhanjan.sharma@spreadd.io>
2023-12-19 14:45:38 +0530

    creating Typography

---
+++ src/stories/Typograpgy/Typography.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Typography } from "./Typography";
+
+export default{
+    title:"Typography",
+    component:Typography
+}
+
+export const typography=()=><Typography/>
