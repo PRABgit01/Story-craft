4ef3dbcb0d6303ded2860e13d2854baca939003d
prab <prabhanjan.sharma@spreadd.io>
2023-12-20 14:50:20 +0530

    Added Pritter

--- src/stories/Alert/Alert.js
+++ src/stories/Alert/Alert.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { AlertStyles } from "./Alert.style";
 
-
-export const AlertComponent=({color,
-    variant,
-    text,
-    isOutline,
-    border,
-    })=>{
-    return(
-        <AlertStyles colorinput={color} 
-        variant={variant}
-        isOutline={isOutline}
-        border={border}>
-            {text}
-        </AlertStyles>
-    )
-}
+export const AlertComponent = ({ color, variant, text, isOutline, border }) => {
+  return (
+    <AlertStyles
+      colorinput={color}
+      variant={variant}
+      isOutline={isOutline}
+      border={border}
+    >
+      {text}
+    </AlertStyles>
+  );
+};
--- src/stories/Alert/Alert.style.js
+++ src/stories/Alert/Alert.style.js
@@ -1,15 +1,15 @@
-import styled,{css} from "styled-components";
+import styled, { css } from "styled-components";
 import theme from "../theme";
 
-export const AlertStyles=styled.button`
-width: 100%;
-border-radius: 2%;
-text-align: left;
-border:  1px solid;
-padding: 10px 20px;
-font-family: "Rubik", sans-serif;
-gap: 1rem;
-${({ colorinput, isOutline, border }) => css`
+export const AlertStyles = styled.button`
+  width: 100%;
+  border-radius: 2%;
+  text-align: left;
+  border: 1px solid;
+  padding: 10px 20px;
+  font-family: "Rubik", sans-serif;
+  gap: 1rem;
+  ${({ colorinput, isOutline, border }) => css`
     background-color: ${isOutline ? "transparent" : theme?.[colorinput]};
     color: ${isOutline ? theme?.[colorinput] : theme.white};
     border-color: ${border ? "transparent" : theme?.[colorinput]};
@@ -23,12 +23,10 @@ ${({ colorinput, isOutline, border }) => css`
     border-color: ${border ? "transparent" : theme?.[colorinput]};
   `}
   cursor: pointer;
- 
+`;
 
-`
-
-export const AlertContainer=styled.div`
-display: flex;
+export const AlertContainer = styled.div`
+  display: flex;
   flex-wrap: wrap;
   gap: 10px;
-`
+`;
