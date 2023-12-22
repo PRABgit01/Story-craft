1daa8fa5919c4c71c8510b3fa74110f8969e70ea
prab <prabhanjan.sharma@spreadd.io>
2023-12-22 12:46:05 +0530

    made few changes in the h tags

--- src/stories/Typograpgy/Typography.js
+++ src/stories/Typograpgy/Typography.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { TextType } from "./Typography.style";
 
-const Typography = ({ htmltag ,text,type,value }) => {
-  return (<TextType as={htmltag} fontSize={value} type={type} >{text}  </TextType>);
+const Typography = ({ htmltag, text, type,  }) => {
+  return (
+    <TextType as={htmltag}  type={type}>
+      {text}
+    </TextType>
+  );
 };
 
 export default Typography;
--- src/stories/Typograpgy/Typography.style.js
+++ src/stories/Typograpgy/Typography.style.js
@@ -1,23 +1,54 @@
 import styled, { css } from "styled-components";
 import theme from "../theme";
 
-export const TextType = styled.div`
-    color:${({type})=> theme?.[type]};
-    line-height: 1.05em;
-     
-  ${({ as }) => css`
-    line-height: 1.5;
-    font-weight: 600;
-    font-family: "Rubik", sans-serif; 
-  `};   
-
-  ${({ fontSize }) => css`
-    font-size: ${fontSize}rem;
-  `};
+const baseStyles = css`
+  color: ${({ type }) => theme?.[type]};
+  font-weight: bold;
+  font-family: sans-serif;
 `;
 
-export const TextContainer=styled.div`
-display: flex;
+const size = {
+  h1: {
+    fontSize: "3rem",
+    lineHeight: "2.05rem",
+    fontWeight: "800",
+  },
+  h2: {
+    fontSize: "2.5rem",
+    lineHeight: "1.2rem",
+    fontWeight: "700",
+  },
+  h3: {
+    fontSize: "1.8rem",
+    lineHeight: "1.3rem",
+    fontWeight: "600",
+  },
+  h4: {
+    fontSize: "1.4rem",
+    lineHeight: "1.4rem",
+    fontWeight: "500",
+  },
+  h5: {
+    fontSize: "1.2rem",
+    lineHeight: "1.5rem",
+    fontWeight: "500",
+  },
+  h6: {
+    fontSize: "1rem",
+    lineHeight: "1.6rem",
+    fontWeight: "400",
+  },
+};
+
+export const TextType = styled.div`
+  ${baseStyles}
+  font-size: ${({ as }) => size?.[as]?.fontSize};
+  line-height: ${({ as }) => size?.[as]?.lineHeight};
+  font-weight: ${({ as }) => size?.[as]?.fontWeight};
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
